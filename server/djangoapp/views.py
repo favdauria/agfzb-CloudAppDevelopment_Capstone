07c68d53ce1bdc08ac2fc52d6679167a6525a9b3
favdauria <dauriafhc@gmail.com>
2023-04-09 16:47:34 -0400

    login

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -48,9 +48,9 @@ def login_request(request):
         if user is not None:
             # If user is valid, call login method to login current user
             login(request, user)
-            return redirect('djangoapp/')
+            return redirect('djangoapp:index')
         else:
-            # If not, return to login page again
+            # If not, return to main page again
             return render(request, 'djangoapp/', context)
     else:
         return render(request, 'djangoapp/', context)
@@ -63,7 +63,7 @@ def logout_request(request):
     print("Log out the user `{}`".format(request.user.username))
     # Logout user in the request
     logout(request)
-    # Redirect user back to course list view
+    # Redirect user back to main page
     return redirect('djangoapp:index')
 
 # Create a `registration_request` view to handle sign up request
